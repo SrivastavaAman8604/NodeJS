88a6405bb8f82023b96078bcc0e3a7617dbf7da4
SrivastavaAman8604 <aman.srivastava2639@gmail.com>
2024-05-26 08:35:35 +0530

    added .env file and and mention it in gitignore

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config();
 //Define the Mongodb database connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels' 
+// const mongoURL = 'mongodb://localhost:27017/hotels' 
+// const mongoDBURL = process.env.DB_URL_LOCAL 
+const mongoURL = process.env.DB_URL 
 
 //setup mongodb connection
 mongoose.connect(mongoURL,{
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const db = require('./db')
+require('dotenv').config();
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json()); //data is stored in (req.body). 
@@ -19,6 +20,8 @@ app.use('/person',personRoutes)
 const menuRoutes = require('./routes/menuRoutes')
 app.use('/menu', menuRoutes)
 
-app.listen(3000, ()=>{
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
     console.log('server is listening on port 3000')
 })
